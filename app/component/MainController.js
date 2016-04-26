21d67d11aa3558b7c0da0b1a356da884ad59f355
joe <loseca@yahoo.com>
2016-04-26 10:33:33 -0600

    p

---
+++ app/component/MainController.js
@@ -0,0 +1,15 @@
+app.controller("MainController",function(){
+    
+    var mc = this;
+    
+     mc.counter = 0;
+        mc.changeCount = function(change) {
+           mc.counter += change;
+        };
+ 
+ 
+ 
+ 
+ 
+    
+})
